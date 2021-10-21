75b160f7d3a8c56f8e362cdd09865ab122881c1e
Xinze Yang <xinyang150@fordham.edu>
2021-10-21 14:13:14 -0400

    V 0.0.1C

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <h1>{{ name }}</h1>  
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -36,6 +37,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      name:'Hello World!'
+    }
   }
 }
 </script>
