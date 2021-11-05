2d53bdb8a9bd91c6c9dab2739fa10590bce0266f
AkagiMilia <zexinyang@163.com>
2021-11-05 18:07:29 -0400

    Test JSON

--- src/data/paraFetch.py
+++ src/data/paraFetch.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sqlite3
 import os
+import json
 
 con = sqlite3.connect('src/data/database.db')
 cur = con.cursor()
@@ -13,6 +14,40 @@ def load(path, index='name'):
 def toDB(csvPath, tableName, con):
     df = load(csvPath)
     df.to_sql(tableName, con, if_exists='replace')
+
+def getGuidance(type='encoder', format='libx264'):
+    result = os.popen('ffmpeg -h {0}={1}'.format(type,format))
+    parameters = {}
+    paraName = ''
+    subValue = ''
+    isStart = False
+    for line in result.readlines():
+        line = line.strip('\n')
+        line = line.split(' ')
+        line = list(filter(lambda x : x!='', line))
+        print(line)
+        if not isStart or not line:
+            if len(line)>0 and line[-1] == 'AVOptions:':
+                isStart = True
+            continue
+        name = line[0] if line else 0
+        if name and name[0] == '-':
+            paraName = name
+            parameters[name] = {}
+            parameters[name]['valueType'] = line[1][1:-1] if len(line)>1 else None
+            parameters[name]['explanation'] = ' '.join(line[3:]) if len(line)>3 else None
+        else:
+            if 'subValues' not in parameters[paraName]:
+                parameters[paraName]['subValues'] = {}
+            parameters[paraName]['subValues'][name] = {}
+            parameters[paraName]['subValues'][name]['value'] = line[1] if len(line)>1 else None
+            parameters[paraName]['subValues'][name]['explanation'] = ' '.join(line[3:]) if len(line)>3 else None
+    """ parameters = json.dumps(parameters, indent=4)
+    print(parameters) """
+    return parameters
+
+        
+
 
 if __name__ == '__main__':
     # toDB('src/data/libx265.csv', 'libx265', con)
@@ -33,5 +68,13 @@ if __name__ == '__main__':
     # cur.execute("select * from SQLite_master")
     # for line in cur.fetchall():
     #     print(line)
-    result = os.system('ffmpeg -h')
-    print(result)
+    f = open('src/data/Guidance.json','x',encoding='utf-8')
+    guidance = {}
+    guidance['libx264'] = getGuidance(format='libx264')
+    guidance['libx265'] = getGuidance(format='libx265')
+    guidance['libaom-av1'] = getGuidance(format='libaom-av1')
+    guidance['flac'] = getGuidance(format='flac')
+    guidance = json.dumps(guidance, indent=4)
+    print(guidance)
+    f.write(guidance)
+    f.close()
