d71e89a0267ac0f1ad33b0559f048cce728eade0
AkagiMilia <zexinyang@163.com>
2021-11-16 20:12:13 -0500

    Update GuidanceData

--- src/data/paraFetch.py
+++ src/data/paraFetch.py
@@ -15,17 +15,46 @@ def toDB(csvPath, tableName, con):
     df = load(csvPath)
     df.to_sql(tableName, con, if_exists='replace')
 
-def getGuidance(type='encoder', format='libx264'):
-    result = os.popen('ffmpeg -h {0}={1}'.format(type,format))
-    parameters = {}
-    paraName = ''
-    subValue = ''
+def getEncoders():
+    result = os.popen('ffmpeg -encoders')
+    encoders = {}
+    videos = {}
+    audios = {}
+    subtitles = {}
     isStart = False
     for line in result.readlines():
         line = line.strip('\n')
         line = line.split(' ')
         line = list(filter(lambda x : x!='', line))
-        print(line)
+        if not isStart or not line:
+            if len(line)>0 and line[0] == '------':
+                isStart = True
+            continue
+        encoderName = line[1]
+        explanation = ' '.join(line[2:])
+        if line[0][0] == 'V':
+            videos[encoderName] = {'explanation':explanation}
+        elif line[0][0] == 'A':
+            audios[encoderName] = {'explanation':explanation}
+        else:
+            subtitles[encoderName] = {'explanation':explanation}
+    encoders['videos'] = videos
+    encoders['audios'] = audios
+    encoders['subtitles'] = subtitles
+    return encoders
+
+
+
+def getGuidance(type='encoder', format='libx264'):
+    result = os.popen('ffmpeg -h {0}={1}'.format(type,format))
+    parameters = {}
+    paraName = ''
+    paraType = ''
+    isStart = False
+    for line in result.readlines():
+        line = line.strip('\n')
+        line = line.split(' ')
+        line = list(filter(lambda x : x!='', line))
         if not isStart or not line:
             if len(line)>0 and line[-1] == 'AVOptions:':
                 isStart = True
@@ -34,14 +63,18 @@ def getGuidance(type='encoder', format='libx264'):
         if name and name[0] == '-':
             paraName = name
             parameters[name] = {}
-            parameters[name]['valueType'] = line[1][1:-1] if len(line)>1 else None
+            paraType = parameters[name]['valueType'] = line[1][1:-1] if len(line)>1 else None
             parameters[name]['explanation'] = ' '.join(line[3:]) if len(line)>3 else None
         else:
             if 'subValues' not in parameters[paraName]:
                 parameters[paraName]['subValues'] = {}
             parameters[paraName]['subValues'][name] = {}
-            parameters[paraName]['subValues'][name]['value'] = line[1] if len(line)>1 else None
-            parameters[paraName]['subValues'][name]['explanation'] = ' '.join(line[3:]) if len(line)>3 else None
+            if paraType.count('flag'):
+                parameters[paraName]['subValues'][name]['value'] = name if len(line)>1 else None
+                parameters[paraName]['subValues'][name]['explanation'] = ' '.join(line[2:]) if len(line)>3 else None
+            else:
+                parameters[paraName]['subValues'][name]['value'] = line[1] if len(line)>1 else None
+                parameters[paraName]['subValues'][name]['explanation'] = ' '.join(line[3:]) if len(line)>3 else None
     """ parameters = json.dumps(parameters, indent=4)
     print(parameters) """
     return parameters
@@ -50,31 +83,23 @@ def getGuidance(type='encoder', format='libx264'):
 
 
 if __name__ == '__main__':
-    # toDB('src/data/libx265.csv', 'libx265', con)
-    # toDB('src/data/subValue.csv', 'subValue', con)
-    # toDB('src/data/libx264.csv', 'libx264', con)
-    # toDB('src/data/formats.csv', 'formats', con)
 
-    # df = pd.read_sql_query("SELECT * FROM libx265", con)
-    # print(df)
-
-    # sql = '''
-    # select explanation from subValue
-    # where name = 'me';
-    # '''
-    # cur.execute(sql)
-    # print(cur.fetchall()[0][0])
-
-    # cur.execute("select * from SQLite_master")
-    # for line in cur.fetchall():
-    #     print(line)
+    encoders = getEncoders()
+    fEncoder = open('public/Encoders.json','x',encoding='utf-8')
+    fEncoder.write(json.dumps(encoders, indent=4))
+    fEncoder.close()
     f = open('src/data/Guidance.json','x',encoding='utf-8')
     guidance = {}
+    for key, value in encoders.items():
+        for encoder in value.keys():
+            guidance[encoder] = getGuidance(format=encoder)
+    
+    """ 
     guidance['libx264'] = getGuidance(format='libx264')
     guidance['libx265'] = getGuidance(format='libx265')
     guidance['libaom-av1'] = getGuidance(format='libaom-av1')
-    guidance['flac'] = getGuidance(format='flac')
+    guidance['flac'] = getGuidance(format='flac') 
+    """
     guidance = json.dumps(guidance, indent=4)
-    print(guidance)
     f.write(guidance)
-    f.close()
+    f.close() 
