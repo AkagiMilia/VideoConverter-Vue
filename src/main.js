ed727d73b3236f211f1706d6e604e35a644a139e
Xinze Yang <xinyang150@fordham.edu>
2021-11-18 02:50:44 -0500

    Reduce Ant Design Import

--- src/main.js
+++ src/main.js
@@ -1,17 +1,46 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue'
+// import Antd from 'ant-design-vue'
+import { 
+  Button, 
+  Card,
+  Col,
+  Row,
+  Checkbox,
+  Icon,
+  Input,
+  Tabs,
+  Tag,
+  AutoComplete,
+  Modal,
+  Steps,
+  Space
+} from 'ant-design-vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/antd.css'
 import store from './store/index'
 import VueVirtualScroller from 'vue-virtual-scroller'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import infiniteScroll from 'vue-infinite-scroll'
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 // var sqlite3 = require('sqlite3').verbose();
 import sqlite3 from 'sqlite3'
 // var db = new sqlite3.Database('./src/data/database.db');
-Vue.use(Antd)
+// Vue.use(Antd)
+Vue.use(Button) 
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Checkbox)
+Vue.use(Icon)
+Vue.use(Input)
+Vue.use(Tabs)
+Vue.use(Tag)
+Vue.use(AutoComplete)
+Vue.use(Modal)
+Vue.use(Steps)
+Vue.use(Space)
+
 Vue.use(VueVirtualScroller)
 Vue.use(infiniteScroll)
 
